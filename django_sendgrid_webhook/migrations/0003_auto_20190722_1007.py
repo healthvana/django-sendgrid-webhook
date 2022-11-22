e48b95fda1f2ca09b81c54b98a4a3e25c7dd3838
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-11-22 11:29:52 -0700

    Revert the app label back to "sendgrid."

--- django_sendgrid_webhook/migrations/0003_auto_20190722_1007.py
+++ django_sendgrid_webhook/migrations/0003_auto_20190722_1007.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_sendgrid_webhook', '0002_email_reason'),
+        ('sendgrid', '0002_email_reason'),
     ]
 
     operations = [
