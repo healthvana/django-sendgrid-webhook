5b562fdeb375099fc45e44db827dcfc7b7f7046d
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-11-23 10:06:48 -0700

    Revert the app label back to "sendgrid." (#2)

---
+++ django_sendgrid_webhook/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class DjangoSendgridWebhook(AppConfig):
+    name = "django_sendgrid_webhook"
+    verbose_name = "Django Sendgrid Webhook"
+    label = "sendgrid"
